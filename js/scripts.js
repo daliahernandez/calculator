dd5a473e17863465f2dcf69fe68dcd205628c5ff
dalia hernandez <daliahdzr98@gmail.com>
2018-11-12 15:42:28 -0800

    addd function that adds two numbers and returns the added value

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+var numberOne = parseInt(prompt("Enter a number"));
+var numberTwo = parseInt(prompt("Enter a Second number"));
+
 var add = function(numberOne, numberTwo){
   return numberOne + numberTwo;
 }//this funtion will return a number that were added together.
+ alert(add(numberOne, numberTwo));
