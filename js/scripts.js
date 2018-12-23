34084a116cb3474d715e55cf8f7d3b5eccb2f605
dalia hernandez <daliahdzr98@gmail.com>
2018-12-22 19:53:06 -0800

    add 4 different forms that subtracts, divides, multiplies, and adds numbers. then create functions for each operation, then displayed it on the UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('#userAnswer form').submit(function(event){
+  $('#userAnswerAdd form').submit(function(event){
     var numOne = parseInt($('input#numOne').val());
     var numTwo = parseInt($('input#numTwo').val());
 
@@ -7,10 +7,43 @@ $(document).ready(function(){
       var answer = numOne + numTwo
       return answer
     }
-
     $('.add').text(add())
-
     event.preventDefault();
-
   })
-})
+
+  $('#userAnswersub form').submit(function(event){
+    var num1 = $('input#num1').val();
+    var num2 = $('input#num2').val();
+
+    function sub(){
+      var result = num1 - num2
+      return result
+    }
+    $('.sub').text(sub());
+    event.preventDefault();
+});
+
+$('#userAnswermult form').submit(function(event){
+  var num1 = $('input#numberOne').val();
+  var num2 = $('input#numberTwo').val();
+
+  function mult(){
+    var result = num1 * num2
+    return result
+  }
+  $('.mult').text(mult());
+  event.preventDefault();
+});
+
+$('#userAnswerdiv form').submit(function(event){
+  var num1 = $('input#n1').val();
+  var num2 = $('input#n2').val();
+
+  function div(){
+    var result = num1 / num2
+    return result
+  }
+  $('.div').text(div());
+  event.preventDefault();
+});
+});
